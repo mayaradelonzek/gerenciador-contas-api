71f90509c84f30c78752a3d0d8e0ffd2a8afed1e
Mayara Delonzek <mayaradelonzek2@gmail.com>
2022-04-23 19:42:46 -0300

    add regra para não salvar pessoa inativa

--- src/main/java/com/gerenciador/api/resource/LancamentoResource.java
+++ src/main/java/com/gerenciador/api/resource/LancamentoResource.java
@@ -1,16 +1,22 @@
 package com.gerenciador.api.resource;
 
 import com.gerenciador.api.event.RecursoCriadoEvent;
+import com.gerenciador.api.exceptionhandler.GerenciadorExceptionHandler;
 import com.gerenciador.api.model.Lancamento;
 import com.gerenciador.api.repository.LancamentoRepository;
 import com.gerenciador.api.service.LancamentoService;
+import com.gerenciador.api.service.exception.PessoaInexistenteOuInativaException;
+
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,11 +27,13 @@ public class LancamentoResource {
     private LancamentoRepository lancamentoRepository;
     private LancamentoService lancamentoService;
     private ApplicationEventPublisher publisher;
+    private MessageSource messageSource;
 
-    public LancamentoResource(LancamentoRepository lancamentoRepository, LancamentoService lancamentoService, ApplicationEventPublisher publisher) {
+    public LancamentoResource(LancamentoRepository lancamentoRepository, LancamentoService lancamentoService, ApplicationEventPublisher publisher, MessageSource messageSource) {
         this.lancamentoRepository = lancamentoRepository;
         this.lancamentoService = lancamentoService;
         this.publisher = publisher;
+        this.messageSource = messageSource;
     }
 
     @GetMapping
@@ -44,5 +52,13 @@ public class LancamentoResource {
         Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
         publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
         return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+    }
+
+    @ExceptionHandler({PessoaInexistenteOuInativaException.class })
+    public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException exception){
+        String mensagemUsuario = messageSource.getMessage("pessoa.inativa-ou-inexistente", null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = exception.toString();
+        List<GerenciadorExceptionHandler.Erro> erros = Arrays.asList(new GerenciadorExceptionHandler.Erro(mensagemUsuario, mensagemDesenvolvedor));
+        return ResponseEntity.badRequest().body(erros);
     }
 }
--- src/main/java/com/gerenciador/api/service/LancamentoService.java
+++ src/main/java/com/gerenciador/api/service/LancamentoService.java
@@ -2,19 +2,27 @@ package com.gerenciador.api.service;
 
 import com.gerenciador.api.model.Lancamento;
 import com.gerenciador.api.model.Pessoa;
+import com.gerenciador.api.repository.LancamentoRepository;
 import com.gerenciador.api.repository.PessoaRepository;
+import com.gerenciador.api.service.exception.PessoaInexistenteOuInativaException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LancamentoService {
 
     private PessoaRepository pessoaRepository;
+    private LancamentoRepository lancamentoRepository;
 
-    public LancamentoService(PessoaService pessoaRepository) {
+    public LancamentoService(PessoaRepository pessoaRepository, LancamentoRepository lancamentoRepository) {
         this.pessoaRepository = pessoaRepository;
+        this.lancamentoRepository = lancamentoRepository;
     }
 
     public Lancamento salvar(Lancamento lancamento) {
-        Pessoa pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+        Pessoa pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo()).get();
+        if (pessoa == null || pessoa.isInativo()) {
+            throw new PessoaInexistenteOuInativaException();
+        }
+        return lancamentoRepository.save(lancamento);
     }
 }
